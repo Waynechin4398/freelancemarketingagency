6728bee2fec2790d7148a67d660d118abae57a4b
Chin Jun Lin <wayne.chin@Chins-MacBook-Air.local>
2026-08-06 16:36:02 +0800

    Use wayneomni.com as production URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: process.env.PUBLIC_SITE_URL || undefined,
+  site: process.env.PUBLIC_SITE_URL || "https://wayneomni.com",
   output: "static",
   build: {
     assets: "assets",
